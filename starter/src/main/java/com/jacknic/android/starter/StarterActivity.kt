7ea4a236ad5120472dbe8b8f938704c775216d7a
Jacknic <jacknic@qq.com>
2019-04-20 15:52:10 +0800

    调整构件脚本及部分项目内容

--- starter/src/main/java/com/jacknic/android/starter/StarterActivity.kt
+++ starter/src/main/java/com/jacknic/android/starter/StarterActivity.kt
@@ -5,18 +5,18 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_starter.*
 
 
-class MainActivity : AppCompatActivity() {
+class StarterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_starter)
         val packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
         val activities = packageInfo.activities.filter { javaClass.name != it.name }
         val activityNameList = activities.map { it.loadLabel(packageManager) }
-        activityList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, activityNameList)
+        activityList.adapter = ArrayAdapter<CharSequence>(this, android.R.layout.simple_list_item_1, activityNameList)
         activityList.setOnItemClickListener { _, _, position, _ ->
             val activityInfo = activities[position]
             val intent = Intent(this, Class.forName(activityInfo.name))
